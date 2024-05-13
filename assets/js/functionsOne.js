675a157bd4e97b39310a8bb63e64180fb8fa20e9
FrederikWagnerC <frederikwagnercs@gmail.com>
2024-05-13 09:02:56 +0200

    Update JS
    Updated JS for functionsOne, Two and Three

--- assets/js/functionsOne.js
+++ assets/js/functionsOne.js
@@ -2,4 +2,8 @@
 // her kaldes, eller invokes din funktion
 helloWorld();
 
-// her skal du skrive din funktion
+// her skal du skrive din funktion      
+
+function helloWorld() {
+    console.log('hello world')
+}
